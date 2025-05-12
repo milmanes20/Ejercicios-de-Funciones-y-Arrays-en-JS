dfa744cb1e3c7e6f15796ad20ede7d92beaaabf9
Fabio D. Argañaraz A. <ing.fabio.arg@gmail.com>
2025-05-12 12:27:13 -0300

    Create homework.js

---
+++ homework.js
@@ -0,0 +1,242 @@
+// No cambies los nombres de las funciones.
+
+function devolverPrimerElemento(array) {
+  // Devuelve el primer elemento de un array
+  // Tu código:
+}
+
+function devolverUltimoElemento(array) {
+  // Devuelve el último elemento de un array
+  // Tu código:
+}
+
+function obtenerLargoDelArray(array) {
+  // Devuelve el largo de un array
+  // Tu código:
+}
+
+function incrementarPorUno(array) {
+  // "array" debe ser una matriz de enteros (int/integers)
+  // Aumenta cada entero por 1
+  // y devuelve el array
+  // Tu código:
+}
+
+function agregarItemAlFinalDelArray(array, elemento) {
+  // Añade el "elemento" al final del array
+  // y devuelve el array
+  // Tu código:
+}
+
+function agregarItemAlComienzoDelArray(array, elemento) {
+  // Añade el "elemento" al comienzo del array
+  // y devuelve el array
+  // Pista: usa el método `.unshift`
+  // Tu código:
+}
+
+function dePalabrasAFrase(palabras) {
+  // "palabras" es un array de strings/cadenas
+  // Devuelve un string donde todas las palabras estén concatenadas
+  // con espacios entre cada palabra
+  // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
+  // Tu código:
+}
+
+function arrayContiene(array, elemento) {
+  // Comprueba si el elemento existe dentro de "array"
+  // Devuelve "true" si está, o "false" si no está
+  // Tu código:
+}
+
+function agregarNumeros(numeros) {
+  // "array" debe ser una matriz de enteros (int/integers)
+  // Suma todos los enteros y devuelve el valor
+  // Tu código:
+}
+
+function promedioResultadosTest(resultadosTest) {
+  // "resultadosTest" debe ser una matriz de enteros (int/integers)
+  // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
+  // Tu código:
+}
+
+function numeroMasGrande(numeros) {
+  // "numeros" debe ser una matriz de enteros (int/integers)
+  // Devuelve el número más grande
+  // Tu código:
+}
+
+function multiplicarArgumentos() {
+  // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
+  // Si no se pasan argumentos devuelve 0
+  // Si se pasa un argumento, simplemente devuélvelo
+  // Tu código:
+}
+
+// No cambies los nombres de las funciones.
+
+function crearGato(nombre, edad) {
+  // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
+  // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
+  // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
+  // Devuelve el objeto
+  // Tu código:
+}
+
+
+function agregarPropiedad(objeto, property) {
+  // Agrega una propiedad al objeto (argumento "objeto") con el valor `null`
+  // Devuelve el objeto
+  // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
+  // Tu código:
+}
+
+function invocarMetodo(objeto, metodo) {
+  // "metodo" es una cadena que contiene el nombre de un método (funcion) en el objeto
+  // Invoca ese método
+  // Nada necesita ser devuelto ("returned")
+  // Tu código:
+}
+
+function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
+  // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
+  // Multiplica el numeroMisterioso por 5 y devuelve el producto
+  // Tu código:
+
+}
+
+function eliminarPropiedad(objeto, propiedad) {
+  // Elimina la propiedad "propiedad" de "objeto"
+  // Devuelve el objeto
+  // Tu código:
+}
+
+function nuevoUsuario(nombre, email, password) {
+  // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
+  // Devuelve el objeto
+  // Tu código:
+
+}
+
+function tieneEmail(usuario) {
+  // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
+  // De lo contratio, devuelve "false"
+  // Tu código:
+}
+
+
+function tienePropiedad(objeto, propiedad) {
+  // Devuelve "true" si el objeto tiene el valor del argumento "propiedad"
+  // "propiedad" es un string
+  // De lo contrario, devuelve "false"
+  // Tu código:
+}
+
+function verificarPassword(usuario, password) {
+  // Comprueba si la "password" enviada coincide con la propiedad "password" del objeto "usuario"
+  // Devuelve "true" si coinciden
+  // De lo contrario, devuelve "false"
+  // // Tu código:
+}
+
+function actualizarPassword(usuario, nuevaPassword) {
+  // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
+  // Devuelve el objeto
+  // Tu código:
+}
+
+function agregarAmigo(usuario, nuevoAmigo) {
+  // "usuario" tiene una propiedad llamada "amigos" que es un array
+  // Agrega "nuevoAmigo" al final de ese array
+  // Devuelve el objeto "usuario"
+  // // Tu código:
+}
+
+function pasarUsuarioAPremium(usuarios) {
+  // "usuarios" es un array de objetos "usuario"
+  // Cada objeto "usuario" tiene la propiedad "esPremium"
+  // Define cada propiedad "esPremium" de cada objeto como "true"
+  // Devuelve el array de usuarios
+  // Tu código:
+}
+
+function sumarLikesDeUsuario(usuario) {
+  // "usuario" tiene una propiedad llamada "posts" que es un array
+  // "posts" es un array de objetos "post"
+  // Cada objeto "post" tiene una propiedad llamada "likes" que es un entero (int/integer)
+  // Suma todos los likes de todos los objetos "post"
+  // Devuelve la suma
+  // Tu código:
+}
+
+function agregarMetodoCalculoDescuento(producto) {
+  // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
+  // Este método debe multiplicar el "precio" del "producto" ("producto.precio" o "producto[precio]") y "porcentajeDeDescuento" para obtener el descuento
+  // El método resta el descuento del precio y devuelve el precio con descuento
+  // Devuelve el objeto "producto" al final de la función
+  // Ejemplo:
+  // producto.precio -> 20
+  // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
+  // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
+  // Tu código:
+}
+
+// Do not change any of the function names
+
+function invocarCallback(cb) {
+  // Invoca al callback `cb`
+}
+
+function sumarArray(numeros, cb) {
+  // Suma todos los números enteros (int/integers) de un array ("numeros")
+  // Pasa el resultado a `cb`
+  // No es necesario devolver nada
+}
+
+function forEach(array, cb) {
+  // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
+  // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+}
+
+function map(array, cb) {
+  // Crea un nuevo array
+  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+  // El nuevo array debe tener la misma longitud que el array del argumento
+}
+
+// No modificar nada debajo de esta línea
+// --------------------------------
+
+module.exports = {
+  devolverPrimerElemento,
+  devolverUltimoElemento,
+  obtenerLargoDelArray,
+  incrementarPorUno,
+  agregarItemAlFinalDelArray,
+  agregarItemAlComienzoDelArray,
+  dePalabrasAFrase,
+  arrayContiene,
+  agregarNumeros,
+  promedioResultadosTest,
+  numeroMasGrande,
+  multiplicarArgumentos,
+  crearGato,
+  agregarPropiedad,
+  invocarMetodo,
+  multiplicarNumeroDesconocidoPorCinco,
+  eliminarPropiedad,
+  nuevoUsuario,
+  tieneEmail,
+  tienePropiedad,
+  verificarPassword,
+  actualizarPassword,
+  agregarAmigo,
+  pasarUsuarioAPremium,
+  sumarLikesDeUsuario,
+  agregarMetodoCalculoDescuento,
+  invocarCallback,
+  sumarArray,
+  forEach,
+  map,
+};
